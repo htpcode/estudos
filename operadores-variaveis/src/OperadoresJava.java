f1e75f12197c1b7cb6d714614e0c4309a1fed96c
htpcode <jgfmprofissional@gmail.com>
2024-07-03 10:38:07 -0300

    operadores logicos

--- operadores-variaveis/src/OperadoresJava.java
+++ operadores-variaveis/src/OperadoresJava.java
@@ -17,17 +17,36 @@ public class OperadoresJava {
          * > - maior
          * <= - menor igual
          * >= - maior igual
-         */
+         
         int numero1 = 1;
         int numero2 = 2;
 
         boolean simNao = numero1 == numero2;
 
-        System.out.println("numeroUm é igual a numeroDois" + simNao);
+        System.out.println("numeroUm é igual a numeroDois" + simNao);*/
+
+        //operadores lógicos
+
+        /*boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        /* Aqui estamos utilizando o operador lógico E para fazer a união de duas 
+        expressões. 
+        É como se estivesse escrito:
+        "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+        
+
+        if(condicao1 && condicao2)
+            System.out.print("Os dois valores precisam ser verdadeiros");;
+
+        //Se condicao1 OU condicao2 for verdadeira, executar código.
+        if(condicao1 || condicao2)
+            System.out.print("Um dos valores precisa ser verdadeiro");*/
 
         
 
-        
+
 
 
 
