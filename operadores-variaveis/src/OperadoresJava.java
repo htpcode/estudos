ba2fcd5ec5188598ab01f8dd063a79a38f5cd884
htpcode <jgfmprofissional@gmail.com>
2024-07-03 09:59:19 -0300

    operadores boolean

--- operadores-variaveis/src/OperadoresJava.java
+++ operadores-variaveis/src/OperadoresJava.java
@@ -1,5 +1,20 @@
 public class OperadoresJava {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /* == - igual
+         * != - diferente
+         * < - menor
+         * > - maior
+         * <= - menor igual
+         * >= - maior igual
+         */
+        // operaadores boolean
+
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ?"verdadeiro" : "false" ;
+
+        System.out.println(resultado);
     }
 }
