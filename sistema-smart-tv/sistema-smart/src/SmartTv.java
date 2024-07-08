82b4c3e0b04e02e95f98b909d6e237dc983cb837
htpcode <jgfmprofissional@gmail.com>
2024-07-08 14:19:14 -0300

    alteracoes

--- sistema-smart-tv/sistema-smart/src/SmartTv.java
+++ sistema-smart-tv/sistema-smart/src/SmartTv.java
@@ -5,4 +5,20 @@ public class SmartTv {
         int canal = 1;
         int volume = 25;
 
+        public void aumentarVolume(){
+                // aumentar volume +1
+                volume++;
+        }
+        public void diminuirVolume(){
+                // diminuir volume em 1
+                volume--;
+        }
+
+        public void ligar(){
+                ligada = true;
+        }
+        public void desligar(){
+                ligada = false;
+        }
+
 }
